1cc97326064b96595d637260f20bf8d4dcbef888
sgwem107 <sgwem107@outlook.com>
2025-09-02 11:26:16 +0530

    FIx app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,12 @@ import { dataSourceOptions } from 'db/dataSource';
 // import { AuthModule } from './auth/auth.module';
 // import { CompanyModule } from './company/company.module';
 import { TestModule } from './test/test.module';
+import { UserManagementModule } from './user-management/user-management.module';
+import { AuthModule } from './auth/auth.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions) ,TestModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserManagementModule, AuthModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
