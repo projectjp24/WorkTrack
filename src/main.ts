7e16365d9fc93e7b58f9f7a55bdac5f2619e96bc
sgwem107 <sgwem107@outlook.com>
2025-09-02 19:39:33 +0530

    Fix Cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: 'http://localhost:5173', // React frontend origin
+    origin:"https://worktrack.softgoway.in", // React frontend origin
     credentials: true,  
   });
 
