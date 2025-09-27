c397bb1f314486a20f744dbd4626f3bfe7d20a2f
sgwem107 <sgwem107@outlook.com>
2025-09-27 12:59:00 +0530

    Fix:db

--- db/dataSource.ts
+++ db/dataSource.ts
@@ -12,7 +12,7 @@ export const dataSourceOptions: DataSourceOptions={
      entities: ['dist/**/*.entity{.ts,.js}'],
      // migrations: ['dist/db/migration/*{.ts,.js}'],
      logging: false,
-     synchronize: true
+     synchronize: false
 }
 
 const dataSource = new DataSource(dataSourceOptions);
